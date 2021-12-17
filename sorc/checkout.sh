01558763edee7a8356fa42e623a00c2c883a61c0
JessicaMeixner-NOAA <Jessica.Meixner@noaa.gov>
2021-12-17 12:23:59 -0600

    update model versions

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#set -xue
-set -x
 
 while getopts "om:" option; do
  case $option in
@@ -31,7 +29,7 @@ echo ufs-weather-model checkout ...
 if [[ ! -d ufs_model.fd ]] ; then
     git clone https://github.com/ufs-community/ufs-weather-model ufs_model.fd >> ${logdir}/checkout-ufs_model.log 2>&1
     cd ufs_model.fd
-    git checkout ${ufs_model_hash:-eb42fb8b37254cf0483cd21ab4c6ccc51abe4786}
+    git checkout ${ufs_model_hash:-c9b399c1163147833dc5caf045151fbc1026a27b}
     git submodule update --init --recursive
     cd ${topdir}
 else
@@ -42,7 +40,7 @@ echo WW3 checkout ...
 if [[ ! -d WW3.fd ]] ; then
     git clone https://github.com/noaa-emc/ww3 WW3.fd >> ${logdir}/checkout-ww3.log 2>&1
     cd WW3.fd
-    git checkout aed0e737bea014664236c910c2b6a1d8097a2d93 
+    git checkout 59dd93939be95e869e8fde7c842c05ebfac11b41
     cd ${topdir}
 else
     echo 'Skip.  Directory WW3.fd already exists.'
